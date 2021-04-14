8f034b7d5f5ccfd640f6a455c07d73e7434f950c
namikoya <nati.wernli@gmail.com>
2021-04-14 10:12:06 +0200

    enable and disable field and buttons correctly

--- schedule.js
+++ schedule.js
@@ -5,6 +5,7 @@ import config from './config.js';
 let profSelect;
 let classSelect;
 let table;
+let defaultPanel;
 let weekpickerInput;
 let btnBack;
 let btnNext;
@@ -18,17 +19,33 @@ $(document).ready(function () {
   profSelect = $('#prof-select');
   classSelect = $('#class-select');
   table = $('#schedule');
+  defaultPanel = $('#default-panel');
   weekpickerInput = $('#weekpicker input');
   btnBack = $('#back');
   btnNext = $('#next');
   currentDate = moment();
 
-  // prepare weekpicker based on local storage
-  if (!config.classExists()) {
-    weekpickerInput.val('');
-  } else {
-    weekpickerInput.val(moment(currentDate).format('W-YYYY'));
+  // prepare fields based on local storage
+  if(config.profExists()) {
+    // disable class select
+    classSelect.prop('disabled', false);
   }
+  if (config.classExists()) {
+    // enable weekpicker
+    weekpickerInput.prop('disabled', false);
+    // enable buttons
+    btnBack.prop('disabled', false);
+    btnNext.prop('disabled', false);
+  } 
+  else {
+    // disable weekpicker
+    weekpickerInput.prop('disabled', true);
+    // disable buttons
+    btnBack.prop('disabled', true);
+    btnNext.prop('disabled', true);
+  }
+  // set weekpicker value
+  weekpickerInput.val(moment(currentDate).format('W-YYYY'));
   // fill profession options
   fillProfData();
 
@@ -48,7 +65,14 @@ $(document).ready(function () {
 function handleProfSelect() {
   // empty previous rows and fields
   table.find('tbody').empty();
-  weekpickerInput.val('');
+  defaultPanel.hide();
+  // disable weekpicker to prevent errors
+  weekpickerInput.prop('disabled', true);
+  // disable buttons
+  btnBack.prop('disabled', true);
+  btnNext.prop('disabled', true);
+  // enable class select
+  classSelect.prop('disabled', false);
   // set and clear localstorage
   config.setProfession(profSelect.children('option:selected').val());
   config.clearClass();
@@ -60,8 +84,11 @@ function handleProfSelect() {
  * is invoked when class changes
  */
 function handleClassSelect() {
-  // fill weekpicker
-  weekpickerInput.val(moment(currentDate).format('W-YYYY'));
+  // enable weekpicker
+  weekpickerInput.prop('disabled', false);
+  // enable buttons
+  btnBack.prop('disabled', false);
+  btnNext.prop('disabled', false);
   // set localstorage
   config.setClass(classSelect.children('option:selected').val());
   // fill table info
@@ -73,7 +100,7 @@ function handleClassSelect() {
  */
 function handleBackButton() {
   // check if value is not empty
-  if(weekpickerInput.val() != '') {
+  if(!weekpickerInput.is(':disabled')) {
     // calculate one week back and set input value
     weekpickerInput.val(moment(currentDate).subtract(1, 'week').format('W-YYYY'));
     // set global date variable
@@ -88,7 +115,7 @@ function handleBackButton() {
  */
 function handleNextButton() {
   // check if value is not empty
-  if(weekpickerInput.val() != '') {
+  if(!weekpickerInput.is(':disabled')) {
     // calculate one week forward and set input value
     weekpickerInput.val(moment(currentDate).add(1, 'week').format('W-YYYY'));
     // set global date variable
@@ -135,9 +162,11 @@ async function fillClassData() {
   // create select options
   createClassElementsWith(data);
 
-  // enable class select and animate
-  classSelect.prop('disabled', false);
-  if(!config.profExists()) classSelect.effect("shake", {times:2}, 400);
+  // enable class select and animate when data is present
+  if(data.length != 0) {
+    classSelect.prop('disabled', false);
+    if(!config.classExists()) classSelect.effect("shake", {times:2}, 400);
+  } else classSelect.prop('disabled', true);
 
   // fill table with data when local storage exists
   if (config.classExists()) {
@@ -151,7 +180,6 @@ async function fillClassData() {
  * fetches data and fills table with data
  */
 async function fillScheduleData() {
-  const defaultPanel = $('#default-panel');
   const classId = classSelect.children('option:selected').val();
   const week = weekpickerInput.val();
   // fetch data
@@ -159,9 +187,7 @@ async function fillScheduleData() {
   // animations
   if(defaultPanel.is(':visible')) {
     await defaultPanel.fadeOut('normal').promise();
-  } else {
-    await table.find('tbody tr').fadeOut('normal').promise();
-  }
+  } else await table.find('tbody tr').fadeOut('normal').promise();
   // empty previous rows
   table.find('tbody').empty();
   // fills data into table
@@ -189,12 +215,13 @@ function createClassElementsWith(array) {
   // clear options
   classSelect.find('option').remove();
   // create new options per entry
-  if (array.length <= 0) {
+  if (array.length === 0) {
+    // create default option
     generateOption('default', 'No options yet', classSelect);
-    // disable class select
-    classSelect.prop('disabled', true);
   } else {
-    generateOption('default', 'Select Class...', classSelect);
+    // create default option
+    generateOption('default', 'Select Class', classSelect);
+    // create options with data
     for (let item of array) {
       generateOption(item.klasse_id, item.klasse_name, classSelect);
     }
